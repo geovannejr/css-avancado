18292c7b74fa2ee122ab9ff4a31cce6dd9a1ee81
Geovanne Rosas Ferreira Junior <75882@PROWAY.TREINA>
2025-02-12 22:03:38 -0300

    Dados dinamicos componentes student

--- sistema-alunos-proway/src/app/app.component.ts
+++ sistema-alunos-proway/src/app/app.component.ts
@@ -8,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sistema-alunos-proway';
+  listaAluno=[
+ {
+  nome:'Guilherme',
+  curso:'Angular',
+   matricula:234,
+   email:'gulherme@proway.com',
+   status:true,
+
+
+
+ },
+ {
+  nome:'Joao',
+  curso:'Angular',
+   matricula:235,
+   email:'joao@proway.com',
+   status:true,
+
+
+
+ }
+
+  ]    
+  
 }
+
+
--- sistema-alunos-proway/src/app/componentes/students/students.component.ts
+++ sistema-alunos-proway/src/app/componentes/students/students.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-students',
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class StudentsComponent {
 
+@Input[]
+aluno:any=[];
+
+
+
+   
 }
